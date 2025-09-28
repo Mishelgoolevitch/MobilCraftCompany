6ad4b0fbec42624c2c060b6c75848aac7e4fa92b
mixail <v1tus5555@yandex.by>
2025-09-28 10:01:05 +0300

    Добавляем interface IServiceCategoriesRepository в папку Domain.Repositories.Abstract

---
+++ MobilCraftCompany/Domain/Repositories/Abstract/IServiceCategoriesRepository.cs
@@ -0,0 +1,13 @@
+﻿using MobilCraftCompany.Domain.Entities;
+
+namespace MobilCraftCompany.Domain.Repositories.Abstract
+{
+    public interface IServiceCategoriesRepository
+    {
+        Task<IEnumerable<ServiceCategory>> GetServiceCategoriesAsync();
+        Task<ServiceCategory> GetServiceCategoryByIdAsync(int id);
+        Task SaveServiceCategoryAsync(ServiceCategory entity);
+        Task DeleteServiceCategoryAsync(int id);
+
+    }
+}
