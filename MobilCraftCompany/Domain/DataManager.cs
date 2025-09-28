d853cd9e0aa2c27b4ca13433fa1807ee1a038675
mixail <v1tus5555@yandex.by>
2025-09-28 13:08:19 +0300

    Создаем класс EFServiceRepository и реализуем интерфейс IServiceRepository в папке Domain.Repositories.EntityFramework

---
+++ MobilCraftCompany/Domain/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using MobilCraftCompany.Domain.Repositories.Abstract;
+
+namespace MobilCraftCompany.Domain
+{
+    public class DataManager
+    {
+        public IServiceCategoriesRepository ServiceCategories {  get; set; }
+        public IServicesRepository Services { get; set; }
+
+        public DataManager( IServiceCategoriesRepository serviceCategoriesRepository, IServicesRepository servicesRepository) 
+        {
+            ServiceCategories = serviceCategoriesRepository;
+            Services = servicesRepository;
+        }
+    }
+}
