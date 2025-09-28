3519b95830b021e4be05f7d448396c602918e945
mixail <v1tus5555@yandex.by>
2025-09-28 10:46:43 +0300

    Создаем класс EFServiceCategoriesRepository и реализуем интерфейс IServiceCategoriesRepository в папке Domain.Repositories.EntityFramework

--- MobilCraftCompany/Domain/Repositories/Abstract/IServiceCategoriesRepository.cs
+++ MobilCraftCompany/Domain/Repositories/Abstract/IServiceCategoriesRepository.cs
@@ -5,7 +5,7 @@ namespace MobilCraftCompany.Domain.Repositories.Abstract
     public interface IServiceCategoriesRepository
     {
         Task<IEnumerable<ServiceCategory>> GetServiceCategoriesAsync();
-        Task<ServiceCategory> GetServiceCategoryByIdAsync(int id);
+        Task<ServiceCategory?> GetServiceCategoryByIdAsync(int id);
         Task SaveServiceCategoryAsync(ServiceCategory entity);
         Task DeleteServiceCategoryAsync(int id);
 
