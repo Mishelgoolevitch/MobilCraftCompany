376c4d6ab5401c6c7782c59e6deab5979e3a96d2
mixail <v1tus5555@yandex.by>
2025-09-29 18:11:21 +0300

    Добавляем сервисы в файл Services в частичный класс partial class AdminController

--- MobilCraftCompany/Controllers/Admin/Core.cs
+++ MobilCraftCompany/Controllers/Admin/Core.cs
@@ -8,9 +8,11 @@ namespace MobilCraftCompany.Controllers.Admin
     public partial class AdminController : Controller
     {
         private readonly DataManager _dataManager;
-        public AdminController(DataManager dataManager)
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public AdminController(DataManager dataManager, IWebHostEnvironment hostingEnvironment)
         {
             _dataManager = dataManager;
+            _hostingEnvironment = hostingEnvironment;
         }
         public async Task<IActionResult> Index()
         { 
@@ -19,5 +21,16 @@ namespace MobilCraftCompany.Controllers.Admin
 
             return View();
         }
+
+        //Сохраняем картинку в файловую систему
+        public async Task<string> SaveImg(IFormFile img)
+        {
+            string path = Path.Combine(_hostingEnvironment.WebRootPath, "images/", img.FileName);
+            await using FileStream stream = new FileStream(path, FileMode.Create);
+            await img.CopyToAsync(stream);
+
+            return path;
+        }
+             
     }
 }
--- MobilCraftCompany/Controllers/Admin/Services.cs
+++ MobilCraftCompany/Controllers/Admin/Services.cs
@@ -1,7 +1,42 @@
-﻿namespace MobilCraftCompany.Controllers.Admin
+﻿using Microsoft.AspNetCore.Mvc;
+using MobilCraftCompany.Domain.Entities;
+
+namespace MobilCraftCompany.Controllers.Admin
 {
     public partial class AdminController
     {
+        public async Task<IActionResult> ServicesEdit(int id)
+        {
+            Service? entity = id == default ? new Service() : await _dataManager.Services.GetServiceByIdAsync(id);
+            ViewBag.ServiceCategoreies = await _dataManager.ServiceCategories.GetServiceCategoriesAsync();
+            return View(entity);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> ServicesEdit(Service entity, IFormFile? titleImageFile)
+        {
+            if (!ModelState.IsValid)
+            { 
+                ViewBag.ServiceCategoreies = await _dataManager.ServiceCategories.GetServiceCategoriesAsync();
+            return View(entity);
+            }
+
+
+            if (titleImageFile != null)
+            {
+                entity.Photo = titleImageFile.FileName;
+                await SaveImg(titleImageFile);
+            }
+
+            await _dataManager.Services.SaveServiceAsync(entity);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServicesDelete(int id)
+        {
+            await _dataManager.Services.DeleteServiceAsync(id);
+            return RedirectToAction("Index");
+        }
     }
 }
