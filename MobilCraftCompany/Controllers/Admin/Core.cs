a84e45a530d20f6298ab042d4afcb41f3ee624f3
mixail <v1tus5555@yandex.by>
2025-10-01 16:13:04 +0300

    Делаем функционал подгрузки изображений и в <script> записываем код в ServicesEdit.html

--- MobilCraftCompany/Controllers/Admin/Core.cs
+++ MobilCraftCompany/Controllers/Admin/Core.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MobilCraftCompany.Domain;
+using System.Text.Json;
 
 namespace MobilCraftCompany.Controllers.Admin
 {
@@ -31,6 +32,34 @@ namespace MobilCraftCompany.Controllers.Admin
 
             return path;
         }
-             
+        //[HttpPost]
+        //public async Task<JsonResult> SaveEditorImg()
+        //{
+        //    if (Request.Form.Files.Count == 0)
+        //        return Json(new { error = "No file uploaded" });
+
+        //    IFormFile img = Request.Form.Files[0];
+
+        //    string folder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+        //    if (!Directory.Exists(folder))
+        //        Directory.CreateDirectory(folder);
+
+        //    string path = Path.Combine(folder, img.FileName);
+        //    await using FileStream stream = new FileStream(path, FileMode.Create);
+        //    await img.CopyToAsync(stream);
+
+        //    return Json(new { location = $"/images/{img.FileName}" });
+        //}
+
+
+       // Сохраняем картинку из редактора
+        public async Task<string> SaveEditorImg()
+        {
+            IFormFile img = Request.Form.Files[0];
+            await SaveImg(img);
+
+            return JsonSerializer.Serialize(new { location = Path.Combine("/images/", img.FileName) });
+        }
+
     }
 }
---
+++ MobilCraftCompany/Models/Components/MenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobilCraftCompany.Domain;
+using MobilCraftCompany.Domain.Entities;
+
+namespace MobilCraftCompany.Models.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly DataManager _dataManager;
+
+            public  MenuViewComponent(DataManager dataManager)
+        {
+            _dataManager= dataManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<Service> list=await _dataManager.Services.GetServicesAsync();
+            return await Task.FromResult((IViewComponentResult) View("Defalt", list));
+        }
+    }
+}
