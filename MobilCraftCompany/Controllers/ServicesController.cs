88aae1a0babe448ccc692b030f236a499d8cb10b
mixail <v1tus5555@yandex.by>
2025-10-02 18:16:41 +0300

    Добавляем контролер ServicesController и страницу представления Index.cshtml

---
+++ MobilCraftCompany/Controllers/ServicesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobilCraftCompany.Domain;
+using MobilCraftCompany.Domain.Entities;
+using MobilCraftCompany.Infrastructure;
+using MobilCraftCompany.Models;
+
+namespace MobilCraftCompany.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly DataManager _dataManager;
+
+        public ServicesController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Service> list = await _dataManager.Services.GetServicesAsync();
+
+            //Доменную сущность на клиенте использовать не рекомендуется, оборачиваем ее в DTO
+
+            IEnumerable<ServiceDTO> listDTO = HelperDTO.TransformServices(list);
+
+            return View(listDTO);
+
+        }
+    }
+}
