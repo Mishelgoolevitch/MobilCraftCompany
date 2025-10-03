e1b4639deeeb5bdb4891b490095d4005aa0aa90b
mixail <v1tus5555@yandex.by>
2025-10-03 09:54:23 +0300

    Добавляем новую функцию  Show в ServicesController.cs

--- MobilCraftCompany/Controllers/ServicesController.cs
+++ MobilCraftCompany/Controllers/ServicesController.cs
@@ -3,6 +3,7 @@ using MobilCraftCompany.Domain;
 using MobilCraftCompany.Domain.Entities;
 using MobilCraftCompany.Infrastructure;
 using MobilCraftCompany.Models;
+using System.Collections.Generic;
 
 namespace MobilCraftCompany.Controllers
 {
@@ -23,7 +24,21 @@ namespace MobilCraftCompany.Controllers
             IEnumerable<ServiceDTO> listDTO = HelperDTO.TransformServices(list);
 
             return View(listDTO);
+        }
 
+        public async Task<IActionResult> Show(int id)
+        {
+            Service? entity=await _dataManager.Services.GetServiceByIdAsync(id);
+
+            //Услуги с данным ID не найдено, отвечаем кодом 404
+            if (entity is null)
+                return NotFound();
+
+            //Доменную сущность на клиенте использовать не рекомендуется, оборачиваем ее в DTO
+
+            Service entityDTO = HelperDTO.TransformServices(entity);
+
+            return View(entityDTO);
         }
     }
 }
